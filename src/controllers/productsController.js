143c1df8b1f4cf7104a0f81f119b011bc38451e9
Caroline LS <carolinelds@gmail.com>
2022-05-15 19:12:48 -0300

    Fix endpoint to get list of products to not ask for user token anymore

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,16 +1,8 @@
 import db from "./../db.js";
 
 export async function getProducts(req, res) {
-    const { authorization } = req.headers;
-    const token = authorization?.replace("Bearer ", "");
 
     try {
-        const session = await db.collection("sessions").findOne({ token });
-        if (!session) {
-            res.sendStatus(401);
-            return;
-        }
-
         const products = await db.collection("products").find({}).toArray();
 
         res.status(200).send(products);
