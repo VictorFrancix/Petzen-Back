3e6118f1a777c4ca05dbb14aec2fb3491ff7759b
Maria Luiza <maria.lmm.975@gmail.com>
2022-05-15 22:43:08 -0300

    feat: add endpoints GET /users and PUT /users

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import { login, signUp } from './../controllers/userController.js';
+import { login, signUp, getUser, editUser } from './../controllers/userController.js';
 
 const userRouter = Router();
 
 userRouter.post("/login", login);
 userRouter.post("/signup", signUp);
+userRouter.get("/users", getUser);
+userRouter.put("/users", editUser);
 
 
 export default userRouter;
