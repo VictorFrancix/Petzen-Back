57444de2d3e520d8fa236b0b168a7f4dcc38626d
Caroline LS <carolinelds@gmail.com>
2022-05-16 06:39:23 -0300

    Solve merge issues

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,95 +4,48 @@ import { v4 as uuid } from "uuid";
 import db from "./../db.js";
 import { loginSchema, signUpSchema, editUserSchema } from "../schemas/userSchemas.js";
 
-function validateToken(token){
-  try {
-    const session = await db.collection("sessions").findOne({ token });
-    console.log;
+async function validateToken(token) {
+    try {
+        const session = await db.collection("sessions").findOne({ token });
+        console.log;
 
-    if (!session) {
-        res.sendStatus(401);
-        return;
+        if (!session) {
+            res.sendStatus(401);
+            return;
+        }
+
+        const user = await db
+            .collection("users")
+            .findOne({ _id: session.userId });
+
+        if (!user) {
+            res.sendStatus(404);
+            return;
+        }
+
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
     }
-
-    const user = await db
-        .collection("users")
-        .findOne({ _id: session.userId });
-
-    if (!user) {
-        res.sendStatus(404);
-        return;
-    }
-
-} catch (e) {
-    console.log(e);
-    res.sendStatus(500);
-}
 }
 
 
-export async function login (req, res) {
+export async function login(req, res) {
     const body = req.body;
-    const validate = loginSchema.validate(body, {abortEarly: false});
+    const validate = loginSchema.validate(body, { abortEarly: false });
 
-    if(validate.error){
-        console.log(validate.error.details.map(detail => detail.message));
-        res.sendStatus(422);
-        return;
-    }
-  
-      try {
-          const user = await db
-              .collection("users")
-              .findOne({ email: body.email });
-  
-          if (user && bcrypt.compareSync(body.password, user.password)) {
-              const token = uuid();
-              await db.collection("sessions").insertOne({
-                  userId: user._id,
-                  token,
-              });
-              const name = user.name;
-              res.status(200).send(token, name);
-          } else {
-              res.sendStatus(401);
-          }
-      } catch (erro) {
-          console.log(erro);
-          res.sendStatus(500);
-      }
-  };
-  
-  export async function signUp (req, res)  {
-    const {email, password, name} = req.body;
-
-    const validate = signUpSchema.validate({email, password, name}, {abortEarly: false});
-    
-    if(validate.error){ 
+    if (validate.error) {
         console.log(validate.error.details.map(detail => detail.message));
         res.sendStatus(422);
         return;
     }
 
     try {
-      const user = await db.collection("users").findOne({email});
-      if (user) {
-        res.sendStatus(409);
-        return;
-      }
-      const hash = bcrypt.hashSync(password, 10);
-      await db.collection("users").insertOne({
-        name,
-        email,
-        password: hash,
-      });
-      res.sendStatus(201);
-      console.log(db.collection("users").findOne({email}));
-    } catch (error) {
-      res.sendStatus(500);
-    }
-    
-  };
+        const user = await db
+            .collection("users")
+            .findOne({ email: body.email });
 
+<<<<<<< HEAD
   export async function getUser (req, res) {
     const { authorization } = req.headers;
     const token = authorization?.replace("Bearer ", "");
@@ -133,11 +86,41 @@ export async function login (req, res) {
     const validate = editUserSchema.validate(body, {abortEarly: false});
 
     if(validate.error){
+=======
+        if (user && bcrypt.compareSync(body.password, user.password)) {
+            const token = uuid();
+            await db.collection("sessions").insertOne({
+                userId: user._id,
+                token,
+            });
+            const name = user.name;
+            const response = {
+                token: token,
+                name: name
+            };
+            res.status(200).send(response);
+        } else {
+            res.sendStatus(401);
+        }
+    } catch (erro) {
+        console.log(erro);
+        res.sendStatus(500);
+    }
+};
+
+export async function signUp(req, res) {
+    const { email, password, name } = req.body;
+
+    const validate = signUpSchema.validate({ email, password, name }, { abortEarly: false });
+
+    if (validate.error) {
+>>>>>>> a61e5a7 (Fix login issues)
         console.log(validate.error.details.map(detail => detail.message));
         res.sendStatus(422);
         return;
     }
 
+<<<<<<< HEAD
     const newUser = {
       email: body.change.email ? body.new.email : body.email,
       name: body.change.name ? body.new.name : body.name,
@@ -180,16 +163,43 @@ export async function login (req, res) {
     }
   }
   export async function logout(req, res){
+=======
+    try {
+        const user = await db.collection("users").findOne({ email });
+        if (user) {
+            res.sendStatus(409);
+            return;
+        }
+        const hash = bcrypt.hashSync(password, 10);
+        await db.collection("users").insertOne({
+            name,
+            email,
+            password: hash,
+        });
+        res.sendStatus(201);
+        console.log(db.collection("users").findOne({ email }));
+    } catch (error) {
+        res.sendStatus(500);
+    }
+
+};
+
+export async function logout(req, res) {
+>>>>>>> a61e5a7 (Fix login issues)
     const { authorization } = req.headers;
     const token = authorization?.replace("Bearer ", "");
 
     validateToken(token);
 
-    try{
-        await db.collection("sessions").deleteOne({token});
+    try {
+        await db.collection("sessions").deleteOne({ token });
         res.sendStatus(200);
     } catch (err) {
         console.log(err);
         res.sendStatus(500);
     }
+<<<<<<< HEAD
   };
+=======
+};
+>>>>>>> a61e5a7 (Fix login issues)
